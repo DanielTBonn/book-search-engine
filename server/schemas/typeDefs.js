663b14b5ee1bf6f3f64dd854240264f131347752
Daniel Bonn <danieltbonn@gmail.com>
2023-11-10 18:58:25 -0600

    added to typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,25 @@
 const typeDefs = `
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        savedBooks: [Book]!
+    }
 
+    type Book {
+        _id: ID
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
+
+    type Query {
+        me: User
+    }
 `;
 
 module.exports = typeDefs;
