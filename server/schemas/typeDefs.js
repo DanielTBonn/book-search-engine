d3576e5f33a4e533151fd9e3321b8bcda1a011c2
Daniel Bonn <danieltbonn@gmail.com>
2023-11-10 19:04:35 -0600

    added auth and mutation to typeDef

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -17,8 +17,18 @@ const typeDefs = `
         title: String
     }
 
+    type Auth {
+        token: ID!
+        user: User
+    }
+
     type Query {
         me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+
     }
 `;
 
