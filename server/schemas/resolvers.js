d320b9c861607610b4c194913ec93b6ab3d70826
Daniel Bonn <danieltbonn@gmail.com>
2023-11-12 00:34:28 -0600

    added login func to mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -10,6 +10,23 @@ const resolvers = {
     },
 
     Mutation: {
+        login: async (parent, {email, password}) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw AuthenticationError;
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw AuthenticationError;
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
+        },
         addUser: async (parent, { username, email, password}) => {
             return User.create({ username, email, password});
         }
